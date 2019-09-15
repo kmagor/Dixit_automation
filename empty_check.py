0a7f2347c5ab4a2de718d4d78cc0d9a148d78a02
kmagor <kemagorar@gmail.com>
2019-09-15 15:17:49 +0200

    UPD: empty_check cleanup + adding medianBlur filtering.
    ADD: threshold2 for median filtering > no little pixel noise
    
    COMM: simplified the console output to keeping only the main msg
    
    REM: canny related stuff, other shapes such as triangle, pentagon, the sablon_pottyok_sok.jpg, two others (black and white, real and temp. ) are used instead.

--- empty_check.py
+++ empty_check.py
@@ -10,15 +10,19 @@ pos_Cir_Y = [0] * 6
 RECT_num_and_content = [[0]*13,[0]*13]
 hanyadik = 0
 
-img = cv2.imread("sablon_pottyok_sok.jpg", cv2.IMREAD_GRAYSCALE)
-_, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#img = cv2.imread("igazi_fekete_pottyokkel.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("sablon_pottyok_black.jpg", cv2.IMREAD_GRAYSCALE)
 
-#canny image modification
-cannyke = cv2.Canny(img,2,70,L2gradient=0)
+_, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+#filterint out the tiny noises
+threshold2=threshold
+cv2.medianBlur(threshold,3, threshold2)
+cv2.imshow("filtered", threshold2)
+
+_, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 #just to check the values in the image.
-print(img)
+#print(img)
 
 for cnt in contours:
     approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
@@ -27,13 +31,10 @@ for cnt in contours:
     y = approx.ravel()[1]
     #print(hanyadik)
     hanyadik = hanyadik +1
-    if len(approx) == 3: ## TRIANGLE
-        cv2.putText(img, "Triangle", (x, y), font, 1, (0))
-
-    elif len(approx) == 4: ## RECTANGLE!!!
+    if len(approx) == 4: ## RECTANGLE!!!
         hanyadik_negyzet = hanyadik_negyzet+1
         cv2.putText(img,str(hanyadik_negyzet-1) + ".Rect.", (x, y-15), font, 1, (0))
-        print(hanyadik_negyzet-1, ". negyzet pozicioja:", x,y)
+        # print(hanyadik_negyzet-1, ". negyzet pozicioja:", x,y)
         cv2.line(img, (x, y-80), (x, y + 700), (55, 0, 0), 2)
         cv2.line(img, (x - 80, y), (x +80, y), (55, 0, 0), 2)
         if hanyadik_negyzet < 14:
@@ -41,10 +42,6 @@ for cnt in contours:
             (RECT_num_and_content[1][hanyadik_negyzet - 1]) = hanyadik-1
             pos_Rec_X[hanyadik_negyzet-2] = x
             pos_Rec_Y[hanyadik_negyzet - 2] = y
-    # elif len(approx) == 5:
-    #     cv2.putText(img, "Pentagon", (x, y), font, 1, (0))
-    # elif 6 < len(approx) < 15:
-    #     cv2.putText(img, "Ellipse", (x, y), font, 1, (0))
     else: ## CIRCLE
         cv2.putText(img, str(hanyadik_karika) + "circle", (x, y+30), font, 1, (0))
         if hanyadik_karika < 7:
@@ -57,21 +54,20 @@ for cnt in contours:
                         if RECT_num_and_content[1][d] == x:
                             print("[[[",hanyadik_karika, ". kör benne van az a " + str(RECT_num_and_content[0][d]) + ". négyzetben ]]]")
 
-        print("KARIKA:",hanyadik_karika, ". karika pozicioja:", x, y)
+        # print("KARIKA:",hanyadik_karika, ". karika pozicioja:", x, y)
         hanyadik_karika = hanyadik_karika + 1
 
 
-print("Körpoziciok:")
-print(pos_Cir_X)
-print(pos_Cir_Y)
-print("Négyzet_pozik:")
-print(pos_Rec_X)
-print(pos_Rec_Y)
-print("--------------")
-
-print(RECT_num_and_content)
+# print("Körpoziciok:")
+# print(pos_Cir_X)
+# print(pos_Cir_Y)
+# print("Négyzet_pozik:")
+# print(pos_Rec_X)
+# print(pos_Rec_Y)
+# print("--------------")
+#
+# print(RECT_num_and_content)
 cv2.imshow("shapes", img)
 cv2.imshow("Threshold", threshold)
-cv2.imshow("Canny", cannyke)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
